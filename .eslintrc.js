4eb6d95e07434ade7a3c1eaeafacbe09a9e0ef3e
Luis Carlos Moraes <luiscarlosicsupply1@gmail.com>
2024-08-15 09:35:22 -0300

    chore: Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const rootDir = __dirname;
+
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:node/recommended',
+        'plugin:promise/recommended',
+    ],
+    env: {
+        browser: false,
+        node: true,
+        es2021: true,
+    },
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: [
+        'import',
+        'node',
+        'promise',
+    ],
+    rules: {
+        'import/order': [
+            'error',
+            {
+                groups: [['builtin', 'external', 'internal']],
+                pathGroups: [
+                    {
+                        pattern: '@/**',
+                        group: 'internal',
+                    },
+                ],
+                pathGroupsExcludedImportTypes: ['builtin'],
+                'newlines-between': 'always',
+                alphabetize: {
+                    order: 'asc',
+                    caseInsensitive: true,
+                },
+            },
+        ],
+        'node/no-unsupported-features/es-syntax': 'off',
+        'node/no-missing-import': 'off',
+        'node/no-unpublished-import': 'off',
+        'promise/always-return': 'off',
+        'promise/catch-or-return': 'off',
+        'promise/no-nesting': 'warn',
+        'promise/no-promise-in-callback': 'warn',
+        'promise/no-callback-in-promise': 'warn',
+    },
+    settings: {
+        'import/resolver': {
+            alias: {
+                map: [
+                    ['tests/*', path.join(rootDir, 'tests')],
+                    ['app/*', path.join(rootDir, 'app')],
+                ],
+                extensions: ['.js', '.json'],
+            },
+        },
+    },
+    overrides: [
+        {
+            files: ['tests/**/*.js'],
+            rules: {
+                'no-unused-expressions': 'off',
+            },
+        },
+    ],
+};
